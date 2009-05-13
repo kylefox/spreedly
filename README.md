5f5dea970215f3d3f72ae9900138369ec25580d0
Jon Smelquist <jon@jmax.local>
2009-05-12 22:39:39 -0600

    Updates for giving Subscribers Complimentary Subscriptions

--- README.md
+++ README.md
@@ -19,6 +19,17 @@ To retrieve a specific subscriber:
 
 	spreedly.Subscriber.find(1234)
 	
+Creating a Complimentary Subscription works slightly different however:
+
+	>>> customer = spreedly.ComplimentarySubscription.create({ 
+	...     'duration_quantity': 30,
+	... 	'duration_units': 'days',
+	... 	'feature_level': 'free' }, { 'subscriber_id':1234 })
+	>>> customer.active
+	True
+
+Two dictionaries are sent, one for the complimentary subscription info and one for the subscriber number.
+	
 For a full list of what the API does, check out the [Spreedly Integration Reference](http://spreedly.com/manual/integration-reference/).  You might find the [Rails Examples](http://spreedly.com/manual/integration-reference/api-access-using-rails/) helpful, as they use the [ActiveResource](http://api.rubyonrails.org/files/vendor/rails/activeresource/README.html) pattern.
 
 If you've never heard of them, here's what the fine Spreedly folks do:
--- spreedly.py
+++ spreedly.py
@@ -3,9 +3,18 @@ from pyactiveresource.activeresource import ResourceMeta, ActiveResource
 class Subscriber(ActiveResource):
     pass
 
-class ComplimentarySubscription(ActiveResource):
-    pass        
-    
+class ComplimentarySubscription(ActiveResource): 
+    @classmethod
+    def create(cls, attributes, prefix_options):
+        """
+        Override create method to accept two parameters since prefix
+        options are required. This simply provides the ComplimentarySubscription.create()
+        interface instead of creating the object manually and calling save()
+        """
+        resource = cls(attributes, prefix_options)
+        resource.save()
+        return resource
+
 RESOURCES = {}
 for r in [Subscriber, ComplimentarySubscription]:
     RESOURCES.update({r.__name__: r})
@@ -13,10 +22,15 @@ for r in [Subscriber, ComplimentarySubscription]:
 class Spreedly(object):
     
     def __init__(self, site):
+        if not site.endswith('/'):
+            site.append('/')
         self.site = site
     
     def __getattr__(self, key):
         if key in RESOURCES:
             klass = RESOURCES[key]
-            setattr(klass, 'site', self.site)
+            if klass == ComplimentarySubscription:
+                setattr(klass, 'site', '%s%s'% (self.site,'subscribers/$subscriber_id/') )
+            else:
+                setattr(klass, 'site', self.site)
             return klass
